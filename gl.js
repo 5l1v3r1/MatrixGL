d29b373442457cea3c9f71dd556c1f64a9f30634
Alex Nichol <aqnichol@li550-95.members.linode.com>
2013-11-05 20:14:19 -0500

    did some reorganizing

--- gl.js
+++ gl.js
@@ -2,7 +2,7 @@ var gl;
 
 function initGL() {
     var canvas = document.getElementById('canvas');
-    gl = canvas.getContext('experimental-webgl');
+    gl = canvas.getContext('experimental-webgl', {antialias: true});
     gl.viewportWidth = canvas.width;
     gl.viewportHeight = canvas.height;
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
